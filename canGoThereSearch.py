928b8c7c238a62d1c86c27dc3fe97c05f4a666fe
Daniel Rosmæl Skauge <2001leinad@gmail.com>
2023-09-11 17:42:38 +1000

    adds can_go_there functionality and unit tests

---
+++ canGoThereSearch.py
@@ -0,0 +1,67 @@
+import search
+from collections import namedtuple
+
+class CanGoThereSearch(search.Problem):  # Or SokobanPuzzle
+    def __init__(self, warehouse_instance, target_position):
+        State = namedtuple('State', ['walls', 'boxes', 'worker'])
+        self.state = State(
+            walls=warehouse_instance.walls,
+            boxes=warehouse_instance.boxes,
+            worker=warehouse_instance.worker
+        )
+        self.target_position = target_position
+        super().__init__(self.state,self.target_position)
+
+    def goal_test(self, state):
+        return state.worker == self.target_position
+
+    def actions(self, state):
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        worker_x, worker_y = state.worker
+        possible_actions = []
+
+        for dx, dy in directions:
+            nx, ny = worker_x + dx, worker_y + dy
+
+            if (nx, ny) in state.walls:
+                continue
+            if (nx, ny) in state.boxes:
+                continue
+
+            direction = (dx, dy)
+            possible_actions.append(direction)
+
+        return possible_actions
+
+    def manhattan_distance_heuristic(self, node):
+        x1, y1 = node.state.worker
+        x2, y2 = self.target_position
+        return abs(x1 - x2) + abs(y1 - y2)
+
+    def result(self, state, action):
+        # Apply the action to get the new state (ignoring box-moving for this part)
+        worker_x, worker_y = state.worker
+        dx, dy = action
+        new_worker_position = (worker_x + dx, worker_y + dy)
+        new_state = state._replace(worker=new_worker_position)
+        return new_state
+    
+
+
+def can_go_there(warehouse, dst):
+    '''
+    Determine whether the worker can walk to the cell dst=(row,column) 
+    without pushing any box.
+    
+    @param warehouse: a valid Warehouse object
+
+    @return
+      True if the worker can walk to cell dst=(row,column) without pushing any box
+      False otherwise
+    '''
+    if not warehouse.is_in_warehouse(dst):
+        return False
+    sub_problem = CanGoThereSearch(warehouse, dst)
+    goal_node = search.astar_graph_search(
+        sub_problem, sub_problem.manhattan_distance_heuristic)
+    return goal_node is not None  # If it's None, we couldn't reach the position
---
+++ testCanGoThereSearch.py
@@ -0,0 +1,49 @@
+import unittest
+from canGoThereSearch import can_go_there, CanGoThereSearch
+from sokoban import Warehouse
+
+class TestCanGoThereSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.warehouse = Warehouse()  # Assume Warehouse is defined elsewhere
+
+        # Walls to make a warehouse with a 3x1 open space
+        self.warehouse.walls = (
+            (0, 0), (1, 0), (2, 0), (3, 0),(4,0),
+            (0, 1),                        (4, 1),
+            (0, 2), (1, 2), (2, 2), (3, 2),(4,2)
+        )
+
+        # Worker's position
+        self.warehouse.worker = (1, 1)
+
+        # Box position
+        self.warehouse.boxes = ((2, 1),)
+
+        # Target position
+        self.target_position = (3, 1)
+
+
+        
+
+
+
+
+    def test_can_go_there(self):
+        self.warehouse.boxes = ((3, 1),)
+        self.target_position = (2, 1)
+
+        result = can_go_there(self.warehouse, self.target_position)
+
+        # Assuming that the worker can actually go there
+        self.assertTrue(result)
+
+    def test_cannot_go_there(self):
+        # Initialize an unreachable target_position
+
+        result = can_go_there(self.warehouse, self.target_position)
+
+        self.assertFalse(result)  # Assuming that the worker cannot go there  
+
+if __name__ == '__main__':
+    unittest.main()
