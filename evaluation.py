b4512bd619a8c234fb60aa19ffa27f72de9d70a0
Daniel Rosmæl Skauge <2001leinad@gmail.com>
2023-10-29 14:31:01 +1000

    adds eval timeout, and code modularity

--- evaluation.py
+++ evaluation.py
@@ -2,56 +2,84 @@ from collections import defaultdict
 from solveSokoban import solve_sokoban_elem, solve_sokoban_macro
 from sokoban import Warehouse
 import os
+from multiprocessing import Process, Queue
 
 
-def evaluate_search_algorithms(search_algorithms, warehouse_limit):
-    # Initialize a data structure for results
+
+def worker_solve(warehouse, solver, search_algorithm, queue):
+    # This function will run in a separate process
+    result = solver(warehouse, search_algorithm)
+    queue.put(result)
+
+
+# Default timeout set to 60 seconds
+def evaluate_warehouse(filename, solver, search_algorithm, timeout=10):
+    # Load the warehouse
+    warehouse = Warehouse()
+    warehouse.load_warehouse(filename)
+
+    # Map to convert solver choice to function
+    solver_map = {
+        "macro": solve_sokoban_macro,
+        "elem": solve_sokoban_elem
+    }
+    chosen_solver = solver_map[solver]
+
+    # Use a Queue to retrieve the result
+    q = Queue()
+    # Create a new process to run the solver
+    p = Process(target=worker_solve, args=(
+        warehouse, chosen_solver, search_algorithm, q))
+    p.start()
+
+    # Wait for the specified timeout
+    p.join(timeout)
+
+    # If process is still alive after the timeout, terminate it
+    if p.is_alive():
+        p.terminate()
+        p.join()
+        result = 'Impossible'
+    else:
+        result = q.get()
+
+    if result == 'Impossible':
+        steps = 'Impossible'
+        path = 'Impossible'
+    else:
+        steps = len(result)
+        path = result
+
+    # Log the result
+    entry = {
+        'warehouse': filename,
+        'steps': steps,
+        'path': path,
+        'algorithm': search_algorithm.__name__,
+        'method': solver,
+    }
+
+    return entry
+
+# This function evaluates multiple warehouses using different configurations of solvers and search algorithms
+def evaluate_search_algorithms(search_algorithms, warehouse_count, solver_choice='None'):
     results = defaultdict(list)
 
     # Get list of warehouse files
-    warehouse_files = [f for f in os.listdir(
-        'warehouses') if f.endswith('.txt')]
-
-    # Sort them to maintain some order, if needed
+    warehouse_files = [f for f in os.listdir('warehouses') if f.endswith('.txt')]
     warehouse_files.sort()
+    warehouse_files = warehouse_files[:warehouse_count]
 
-    # Limit the number of warehouses to test
-    warehouse_files = warehouse_files[:warehouse_limit]
+    solvers = ["macro", "elem"] if solver_choice is None else [solver_choice]
 
-    # Loop through warehouse files
     for warehouse_filename in warehouse_files:
-        full_path = os.path.join('warehouses', warehouse_filename)
-
-        # Initialize and load warehouse
-        warehouse = Warehouse()
-        warehouse.load_warehouse(full_path)
-
-        # Loop through both macro and elem solvers
-        for solver in [solve_sokoban_macro, solve_sokoban_elem]:
-            solver_name = "macro" if solver == solve_sokoban_macro else "elem"
-
-            # Loop through all search algorithms
+        print('warehouse: ', warehouse_filename)
+        for solver in solvers:
+            print('solver: ', solver)
             for search_algorithm in search_algorithms:
-                search_algorithm_name = search_algorithm.__name__
-
-                # Solve the problem
-                result = solver(warehouse, search_algorithm)
-
-                if result == 'Impossible':
-                    steps = 'Impossible'
-                    path = 'Impossible'
-                else:
-                    steps = len(result)
-                    path = result
-
-                # Log the result
-                entry = {
-                    'steps': steps,
-                    'path': path,
-                    'algorithm': search_algorithm_name,
-                    'method': solver_name,
-                }
-
+                print('algo: ', search_algorithm)
+                full_path = os.path.join('warehouses', warehouse_filename)
+                entry = evaluate_warehouse(full_path, solver, search_algorithm)
                 results[warehouse_filename].append(entry)
 
     return results
--- run_eval.py
+++ run_eval.py
@@ -1,9 +1,13 @@
 from search import astar_graph_search, breadth_first_graph_search, depth_first_graph_search
-from evaluation import evaluate_search_algorithms
+from evaluation import evaluate_search_algorithms, evaluate_warehouse
 import json
 
-search_algos = [astar_graph_search, breadth_first_graph_search, depth_first_graph_search]
-results = evaluate_search_algorithms(search_algos, 2)
+if __name__ == '__main__':
+    search_algos = [astar_graph_search,
+                    breadth_first_graph_search, depth_first_graph_search]
+    #results = evaluate_search_algorithms(search_algos, 4, 'macro')
+    results_single = evaluate_warehouse(
+        'warehouses/warehouse_07.txt', 'elem', astar_graph_search,10)
 
-with open("result.json", "w") as file:
-    json.dump(results, file, indent=3)
+    with open("result.json", "w") as file:
+        json.dump(results_single, file, indent=3)
